02969173d0a9f2ec76fa05b984ff874b679a77cd
hyejin <po852alth@naver.com>
2024-04-27 18:02:55 +0900

    기능까지

--- movie.js
+++ movie.js
@@ -1,49 +1,63 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDg2OGQ5Yjg2NTNhMzg3ZWRmNGVjN2ZiZWM1NThmNyIsInN1YiI6IjY2MmEzZDZkZGM4NjQ3MDBhZTUyNzI2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.22Vap4cFQXB9I2ZRnpMhTcXIvfOE1JPo8lRvXDJwibU'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDg2OGQ5Yjg2NTNhMzg3ZWRmNGVjN2ZiZWM1NThmNyIsInN1YiI6IjY2MmEzZDZkZGM4NjQ3MDBhZTUyNzI2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.22Vap4cFQXB9I2ZRnpMhTcXIvfOE1JPo8lRvXDJwibU",
+  },
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => {
-    // fetch 요청이 완료된 후 movieArray에 데이터 할당
-    const movieArray = response.results;
-    console.log(movieArray);
-     
-  // 각 카드 요소에 대해 필요한 요소를 선택하여 값을 설정
-  const cards = document.querySelectorAll(".moviecard");
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    // fetch 요청이 완료된 후 movies에 데이터 할당
+    const movies = response.results;
+    console.log(movies);
 
- cards.forEach((card, index) => {
+    //카드 UI구현
+    const movieSection = document.getElementById("movieSection");
+    const movieCard = document.querySelectorAll(".movieCard");
 
-  card.querySelector('img').src = `https://image.tmdb.org/t/p/w300${movieArray[index].poster_path}`;                                 
-  card.querySelector(".title").textContent = movieArray[index].title;
-  card.querySelector(".overview").textContent = movieArray[index].overview;
-  card.querySelector(".average").textContent = movieArray[index].vote_average;
- 
-});
-   
-    
-    //카드 클릭 이벤트 등록
-    cards.forEach((card, index) => {
-      card.addEventListener("click", function () {
-        // 클릭한 카드의 인덱스를 사용하여 movieArray에서 해당하는 영화 ID를 가져옴
-        const movieId = movieArray[index].id;
-        alert(`ID: ${movieId}`);
+    //카드 만들고 section에 추가하는 함수 
+    function showMovieCard(val = "") {
+      //html 기존카드 지우고 내용비우기
+      while (movieSection.firstChild) {
+        movieSection.removeChild(movieSection.firstChild);
+      }
+      movieCard.innerHTML = "";
+      //배열 반복문 돌면서 조건에 만족하는 카드만들고 append하기 
+      movies.forEach((movie) => {
+        if (movie.title.toLowerCase().includes(val.toLowerCase())) {
+          const card = document.createElement("article");
+          card.classList.add("movieCard");
+          card.innerHTML = `
+            <img src='https://image.tmdb.org/t/p/w300${movie.poster_path}'/>
+            <p>${movie.title}</p>
+            <p>${movie.overview}</p>
+            <p>Rating:${movie.vote_average}</p>`;
+          movieSection.appendChild(card); 
+          
+          //id alert
+          card.addEventListener("click", function () {
+            const movieId = movie.id;
+            alert(`ID: ${movieId}`);
+          });
+        }
       });
+    }
+    showMovieCard();
+
+    // 검색기능
+    const searchInput = document.getElementById("search");
+    const searchBtn = document.getElementById("searchBtn");
+
+    searchBtn.addEventListener("click", function (e) {
+      const val = searchInput.value;
+      e.preventDefault();
+      showMovieCard(val);
     });
-
- 
-
-
-
-
-
-
   })
-  .catch(err => console.error(err));
-
- 
-
+  .catch((err) => console.error(err));
